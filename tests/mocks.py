f52715829b6409a495dd68ad19390a55a0a750b4
Simbul <webmaster@simbul.net>
2009-11-28 23:15:19 +0100

    Added LGPL + copyright header.

--- tests/mocks.py
+++ tests/mocks.py
@@ -1,3 +1,22 @@
+# -*- coding: utf-8 -*-
+
+# Copyright © 2009 Alessandro Morandi (email : webmaster@simbul.net)
+#
+# This file is part of pymeo.
+# 
+# pymeo is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# pymeo is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+# 
+# You should have received a copy of the GNU Lesser General Public License
+# along with pymeo.  If not, see <http://www.gnu.org/licenses/>.
+
 import sys
 sys.path.append('.')
 sys.path.append('..')
